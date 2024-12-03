9f16bf7d6ceb0dfebdc393de5bc639d2901c069e
lucyyanderson <118212009+lucyyanderson@users.noreply.github.com>
2024-12-03 13:00:55 -0500

    Update photometry_behav.m

--- photometry_behav.m
+++ photometry_behav.m
@@ -39,5 +39,56 @@ hold off
 
 
 
+% pyphotometry sampling rate: 130
+window = 1; % window of time around lick to average
+sampling_rate = 130;
+samples = window*sampling_rate;
+zscore_matrix = nan(length(rewarded_times), (samples*2)+1);
+zscore_matrix_non = nan(length(nonrewarded_times), (samples*2)+1);
+
+for j = 1:length(rewarded_times)
+    curr_reward_time = rewarded_times(j);
+    [~, reward_idx] = min(abs(photometryData.timestamps - rewarded_times(j)));
+   % photometryData.timestamps(sampling_rate*window)
+    start_idx = reward_idx - samples;
+    end_idx = reward_idx + samples;
+    if start_idx >= 1 && end_idx <= size(photometryData.timestamps, 2)
+        zscore_matrix(j, :) = sync(start_idx:end_idx, 2);
+
+    end
+end
+
+avg_z_reward = mean(zscore_matrix, 1);
+time = linspace(-window, window, ((samples*2)+1));
+
+for k = 1:length(nonrewarded_times)
+    curr_reward_time = nonrewarded_times(j);
+    [~, reward_idx] = min(abs(photometryData.timestamps - nonrewarded_times(k)));
+    start_idx = reward_idx - samples;
+    end_idx = reward_idx + samples;
+    if start_idx >= 1 && end_idx <= size(photometryData.timestamps, 2)
+        zscore_matrix_non(k, :) = sync(start_idx:end_idx, 2);
+
+    end
+end
+
+avg_z_no_reward = mean(zscore_matrix_non, 1);
+
+% plot 
+figure(2);
+subplot(2,1,1)
+plot(time, avg_z_reward, 'LineWidth', 2);
+xlabel('time (s)');
+ylabel('avg z-score');
+title('Average Z-score Around Rewards');
+grid on;
+
+subplot(2,1,2)
+plot(time, avg_z_no_reward, 'LineWidth', 2);
+xlabel('time (s)');
+ylabel('avg z-score');
+title('Average Z-score Around Non-rewarded Licks');
+grid on;
+
 
 
